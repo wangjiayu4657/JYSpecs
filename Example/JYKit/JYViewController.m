bcdd0d0919994c42951fdec77efef140d56d5fd5
jiayuwang <jiayuwang@upchina.com>
2024-02-28 11:20:25 +0800

    删除无关内容

---
+++ Example/JYKit/JYViewController.m
@@ -0,0 +1,27 @@
+//
+//  JYViewController.m
+//  JYKit
+//
+//  Created by jiayuwang on 02/27/2024.
+//  Copyright (c) 2024 jiayuwang. All rights reserved.
+//
+
+#import "JYViewController.h"
+
+@interface JYViewController ()
+
+@end
+
+@implementation JYViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
